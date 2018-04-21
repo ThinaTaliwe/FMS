3700ed7daf43cb92535439d72a7d7df03769cc2c
ThinaTaliwe <216009615@student.uj.ac.za>
2018-04-21 14:21:53 +0200

    implement Truck class

--- FMS/FMS/App_Code/Truck.cs
+++ FMS/FMS/App_Code/Truck.cs
@@ -5,8 +5,43 @@ using System.Web;
 
 namespace FMS.App_Code
 {
+    /// <summary>
+    /// Truck class for all classes created
+    /// </summary>
     public class Truck
     {
+        // attributes
+        private string ID;
+        private int maxLoad;
+        private int maxSpeed;
 
+        // getter and setter
+        public string getID()
+        {
+            return this.ID;
+        }
+        public int getMaxLoad()
+        {
+            return this.maxLoad;
+        }
+        public int getMaxSpeed()
+        {
+            return this.maxSpeed;
+        }
+
+        public void setID(string ID)
+        {
+            this.ID = ID;
+        }
+        public void setMaxLoad(int maxLoad)
+        {
+            this.maxLoad = maxLoad;
+        }
+        public void setMaxSpeed(int maxSpeed)
+        {
+            this.maxSpeed = maxSpeed;
+        }
+
+        // methods
     }
 }
