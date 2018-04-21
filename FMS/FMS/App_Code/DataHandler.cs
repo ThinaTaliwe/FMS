7624e645aa27f54f47dad6bcd7dfe7077d2375a7
ThinaTaliwe <216009615@student.uj.ac.za>
2018-04-21 14:31:30 +0200

    add DataHandler class

---
+++ FMS/FMS/App_Code/DataHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FMS.App_Code
+{
+    /// <summary>
+    /// Truck class for all classes created
+    /// </summary>
+    public class DataHandler : User
+    {
+            // dataHandler methods
+            public Delivery[] viewPastDeliveries(int weeks)
+            {
+            Delivery[] delArr = new Delivery[weeks];
+
+            return delArr;
+            }
+            public string generateReport(Delivery deliveries)
+        {
+            string report = "";
+
+            return report;
+        }
+            public void validateDelivery(Delivery deliveries)
+        {
+
+        }
+        
+    }
+}
