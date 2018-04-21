07a351d2a5813b3b54f95fc38520d66df9e42f93
ThinaTaliwe <36257885+ThinaTaliwe@users.noreply.github.com>
2018-04-21 12:48:53 +0200

    Update Deliver.cs

--- FMS/FMS/App_Code/Deliver.cs
+++ FMS/FMS/App_Code/Deliver.cs
@@ -8,6 +8,11 @@ namespace FMS.App_Code
 {
     interface Deliver
     {
-
+        public Delivery createDilevery(Truck truck, Driver driver);
+        public Bool assignDriver(Driver driver);
+        public void assignRoute();
+        public Bool assignTruck(Truck truck);
+        public void startDelivery();
+        public void setTrip(String to, String from);
     }
 }
