8f4f63172780294b7e9aa1c1c7b38cba340b16fa
ThinaTaliwe <216009615@student.uj.ac.za>
2018-04-21 13:55:44 +0200

    Add Secrecy.cs, to hash passwords

---
+++ FMS/FMS/App_Code/Secrecy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+// 
+using System.Security.Cryptography;
+
+namespace FMS.App_Code
+{
+    /// <summary>
+    ///  SHA1 is more secure than MD5
+    /// </summary>
+    public static class Secrecy
+    {
+
+        public static string HashPassword(String StrPassword)
+        {
+            SHA1 HashAlgorithm = SHA1.Create();
+            //Byte array to store the returned hashed data
+            byte[] hashedData;
+            //Convert the input string to a byte array and compute the hash.
+            hashedData = HashAlgorithm.ComputeHash(Encoding.Default.GetBytes(StrPassword));
+            //String variable that will store the returned hashed string
+            string hashedPassword = "";
+
+            //Loop through each byte of the hashed data and format each one as a hexadecimal string.
+            for (int i = 0; i < hashedData.Length - 1; i++)
+            {
+                hashedPassword += hashedData[i].ToString("x2");
+            }
+
+            //Return the hexadecimal string.
+            return hashedPassword;
+        }
+    }
+}
