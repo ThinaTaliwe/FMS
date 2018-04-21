39755b0f4016ff6d1b0cfd7a12f086728a0be4bd
ThinaTaliwe <216009615@student.uj.ac.za>
2018-04-21 14:53:22 +0200

    add Supervisor class

---
+++ FMS/FMS/App_Code/Supervisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FMS.App_Code
+{
+        public class Supervisor : User
+        {
+            // supervisor attributes
+            private Delivery delivery;
+
+            // supervisor methods
+            public Driver[] viewDrivers(int numDrivers)
+            {
+                Driver[] driverArr = new Driver[numDrivers];
+                for (int i = 0; i < driverArr.Length - 1; i++)
+                {
+                    Console.WriteLine("Driver " + i + " " + delivery.Driver);
+                    driverArr[i] = new Driver(); 
+                }
+                return driverArr;
+            }
+            public void editDriver(Driver driver)
+            {
+
+            }
+            public bool removeDriver(Driver driver)
+            {
+            return false;
+            }
+
+        }
+
+}
