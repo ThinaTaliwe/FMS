dde26bfa87e2fcd3f474758804ad828beaf3ba87
Mmeli-B <mmeli.b.mavuso@gmail.com>
2018-04-21 15:19:15 +0200

    added non-buggy project

---
+++ FMS/FMS/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace FMS
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DriverREST rest = new DriverREST(1998);
+            Thread t = new Thread(rest.start);
+            t.Start();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# team43
+Colloid
+
+Here lies team 43's third year project.
