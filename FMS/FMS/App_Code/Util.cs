e5865966a0de9763b7a274cabc556029a7f1c5e2
Mmeli-B <mmeli.b.mavuso@gmail.com>
2018-04-22 21:35:48 +0200

    added util.cs

---
+++ FMS/FMS/App_Code/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FMS.App_Code
+{
+    public class Util
+    {
+        /**
+         *  Util class will have static utility methods 
+         * **/
+        public static SqlDataReader query(string request)
+        {
+            /**
+             * query method will be used to query database, returns rows matching request
+             * **/
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ToString());
+            conn.Open();
+            SqlCommand command = new SqlCommand(request, conn);
+            return command.ExecuteReader();
+        }
+    }
+}
