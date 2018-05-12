5dfa7103552a9a3578071e76cf4bb187fe2bd161
Mmeli-B <mmeli.b.mavuso@gmail.com>
2018-05-13 01:02:24 +0200

    created service bound for actities

--- fmsdriverapp2/app/src/main/java/fms/colloid/fmsdriverapp/DriverService.java
+++ fmsdriverapp2/app/src/main/java/fms/colloid/fmsdriverapp/DriverService.java
@@ -2,7 +2,9 @@ package fms.colloid.fmsdriverapp;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.StrictMode;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -10,26 +12,35 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+import static android.os.StrictMode.setThreadPolicy;
+
 public class DriverService extends Service {
 
+    private IBinder bound = new DriverServiceBound();
     private Socket conn = null;
     private Scanner in = null;
     private PrintWriter out = null;
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return bound;
     }
 
     @Override
     public int onStartCommand(Intent intent, int flag, int startID) {
         connect("10.0.2.2", 1998);
-        log("service started");
         return Service.START_STICKY;
     }
 
+    public class DriverServiceBound extends Binder {
+        DriverService getBound() {
+            return DriverService.this;
+        }
+    }
+
     private void connect(String address, int port) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        setThreadPolicy(policy);
         try {
             conn = new Socket(address, port);
             in = new Scanner(conn.getInputStream());
@@ -39,7 +50,7 @@ public class DriverService extends Service {
         }
     }
 
-    private String read() {
+    public String read() {
         return in.nextLine();
     }
 
@@ -54,12 +65,17 @@ public class DriverService extends Service {
         return super.onUnbind(intent);
     }
 
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
     private void send(String text) {
         out.write(text);
         out.flush();
     }
 
-    private void log(String message) {
+    public void log(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
--- fmsdriverapp2/app/src/main/java/fms/colloid/fmsdriverapp/MainActivity.java
+++ fmsdriverapp2/app/src/main/java/fms/colloid/fmsdriverapp/MainActivity.java
@@ -1,14 +1,57 @@
 package fms.colloid.fmsdriverapp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DriverService service;
+    private boolean serviceIsBounded;
+    private ServiceConnection conn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            DriverService.DriverServiceBound bound = (DriverService.DriverServiceBound) iBinder;
+            service = bound.getBound();
+            serviceIsBounded = true;
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            serviceIsBounded = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if(serviceIsBounded) service.log("you doing well");
+                else log("sorry, no sleep for you");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, DriverService.class);
+        startService(intent);
+        bindService(intent, conn, Context.BIND_AUTO_CREATE);
+    }
+
+    public void log(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
