f0724774d74b7273a86df97970d308f818ec460f
Maitso <khamorudu@gmail.com>
2018-08-06 13:15:47 +0200

    Added started and completed to all deliveries

--- FMS/FMS/UpdateDelivery.aspx.cs
+++ FMS/FMS/UpdateDelivery.aspx.cs
@@ -13,7 +13,7 @@ namespace FMS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            var query = "select * from Delivery";
+            var query = "select [from], [to], truck, driver, client, started, completed from Delivery";
             var rows = Util.query(query);
             var HTMLStr = "";
             var num = 0; 
@@ -21,8 +21,25 @@ namespace FMS
             {
                 while (rows.Read())
                 {
-                    num++; 
-                    HTMLStr += "<tr> <td> " + Convert.ToString(rows.GetString(1)) + "</td> <td> " + Convert.ToString(rows.GetString(2)) + "</td> <td> " + Delivery.getAddress(rows.GetString(5)) + "</td> <td> " + Delivery.getAddress(rows.GetString(6)) + "</td> <td> <a href=" + "EditDelivery?order=" + num + "> Edit </a> </tr>";
+                    num++;
+                    String From = Delivery.getAddress(rows.GetString(0));
+                    String To = Delivery.getAddress(rows.GetString(1));
+                    string start, end;
+                    if(!rows.IsDBNull(5))
+                    {
+                        start = Convert.ToString(rows.GetDateTime(5).TimeOfDay);
+                        if (!rows.IsDBNull(6))
+                        {
+                            end = Convert.ToString(rows.GetDateTime(6).TimeOfDay);
+                        }
+                        else end = "Not Completed";
+                    } else
+                    {
+                        start = "Not Started";
+                        end = "Not Applicable";
+                    }
+
+                    HTMLStr += "<tr> <td> " + new Client(rows.GetInt32(4)).getCompany() + "</td> <td> " + Convert.ToString(rows.GetString(2)) + "</td> <td> " + From + "</td> <td> " + To + "</td> <td> " + start + "</td> <td> " + end + "</td> <td> "  + "</td> <td> <a href=" + "EditDelivery?order=" + num + "> Edit </a> </tr>";
                 }
                 tables.InnerHtml = HTMLStr;
             }
